594f2dd0e762194edf4666043617c9cb650df0b6
IceFlom <git@iceflom.de>
2022-02-17 14:37:19 +0100

    fix translation

--- javascript-source/lang/english/discord/ice/ice-dailyCommand.js
+++ javascript-source/lang/english/discord/ice/ice-dailyCommand.js
@@ -1,6 +1,6 @@
 $.lang.register('discord.daily.success', 'you got $1 as your daily reward.');
-$.lang.register('discord.daily.wait', 'your last award is only $1 ago. You can get this reward every $2 hours.');
-$.lang.register('discord.daily.help', 'avaiable commands for admins: setpayout [amount]; setinterval [hours]');
+$.lang.register('discord.daily.wait', 'your last reward is only $1 ago. You can get this reward every $2 hours.');
+$.lang.register('discord.daily.help', 'available commands for admins: setpayout [amount]; setinterval [hours]');
 $.lang.register('discord.daily.payout.updated', 'payout updated, new value: $1');
 $.lang.register('discord.daily.payout.current', 'nothing was changed, current amount of payouts: $1');
 $.lang.register('discord.daily.interval.updated', 'interval updated, new value: $1 hours');
