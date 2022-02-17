c57fc84cab481127596f1b362844c825e6f07886
IceFlom <git@iceflom.de>
2022-02-17 15:59:54 +0100

    init slotmachine changes from old version

--- javascript-source/lang/english/discord/games/games-slotMachine.js
+++ javascript-source/lang/english/discord/games/games-slotMachine.js
@@ -15,7 +15,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-$.lang.register('discord.slotmachine.result.start', '$1: [ $2 $3 $4 ] ');
-$.lang.register('discord.slotmachine.result.win', ' +$1 ');
+$.lang.register('discord.slotmachine.result', '-$1 > [ $2 | $3 | $4 ] > +$5');
+$.lang.register('discord.slotmachine.nopoints', 'you do not have enough points.');
 $.lang.register('discord.slotmachine.rewards.usage', 'Usage: !slot rewards [val1] [val2] [val3] [val4] [val5]. Currently: $1');
 $.lang.register('discord.slotmachine.rewards.success', 'Updated rewards for the slot machine.');
