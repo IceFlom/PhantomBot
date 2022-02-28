045dd944eb0cd2b1ece059c094c729a4fe621e12
IceFlom <git@iceflom.de>
2022-02-28 11:53:44 +0100

    removed old lang files

--- javascript-source/lang/german/commands/commands-ttsCommand.js
+++
@@ -1,8 +0,0 @@
-$.lang.register('ttscommand.usage', 'Mit dem Befehl !tts kannst du deine Nachricht von Hans vorlesen lassen. Kosten: $1 / Zeichen. Du musst mindestens $2 hier gewesen sein. (BETA)');
-$.lang.register('ttscommand.offlinewarning', 'Der Stream ist offline. TTS kann nicht funktionieren.');
-$.lang.register('ttscommand.notenoughpoints', '$1 ist leider nicht genug, du brauchst $2');
-$.lang.register('ttscommand.notenoughtime', 'du bist erst seit $2 hier, $1 sind notwendig.');
-$.lang.register('ttscommand.success', 'Nachricht wird abgespielt, Kosten: $1');
-$.lang.register('ttscommand.discord.usage', 'Mit dem Befehl !tts kannst du deine Nachricht von Hans im Trovo-Stream vorlesen lassen. Um Spam zu verhindern, unterliegt der Befehl einem Delay. Wenn dein Befehl akzeptiert wurde, erhälst du eine entsprechende Bestätigung vom Bot.');
-$.lang.register('ttscommand.discord.success', 'Nachricht wird abgespielt. Verzögerung aufgrund des Stream-Delays beachten!');
-$.lang.register('ttscommand.discord.toolong', 'Deine Nachricht ist zu lang! Das Limit beträgt $1 Zeichen!');
